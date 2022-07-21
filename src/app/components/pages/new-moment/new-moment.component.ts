fc3293fb03ab63a0f9df56a7d68811033ad6d10a
Yuri Correia <yuri_caike@hotmail.com>
2022-07-21 10:33:45 -0300

    primeiro commit

---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+
+import { Moment } from 'src/app/Moment';
+
+import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.css']
+})
+export class NewMomentComponent implements OnInit {
+  btnText = 'Compartilhar!';
+
+  constructor(
+    private momentService: MomentService, 
+    private messagesService: MessagesService,
+    private router: Router
+    ) {}
+
+  ngOnInit(): void {}
+
+  async createHandler(moment: Moment){
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+
+
+    //todo
+    
+
+    //enviar para o service
+    await this.momentService.createMoment(formData).subscribe();
+
+
+    this.messagesService.add('Momento adicionado com sucesso!');
+
+
+    //redirect
+    this.router.navigate(['/']);
+  }
+
+}
